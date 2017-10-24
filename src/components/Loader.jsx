9dcd36beb7e2f5d46095841e4cb733b75db6ad03
Kamyl <kamillcybulski@gmail.com>
2017-10-24 14:28:50 +0200

    Add Loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Spinner from 'react-spinkit';
+
+const Loader = () => (
+  <div className="loader">
+    <Spinner name="circle" color="white" />
+  </div>
+);
+
+export default Loader;
