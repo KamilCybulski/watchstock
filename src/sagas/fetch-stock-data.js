85cdb0066c096e81a93d73c50530109c7a8532f6
Kamyl <kamillcybulski@gmail.com>
2017-10-28 14:08:59 +0200

    Fix comments in fetchStockData saga

--- src/sagas/fetch-stock-data.js
+++ src/sagas/fetch-stock-data.js
@@ -13,15 +13,19 @@ import { fetchStockDataSuccess, fetchStockDataFailure } from '../actions/fetch-s
 export function* fetchStockData(action) {
   try {
     if (action.payload.length === 0) {
+      // Dispatch success action with empty object when no symbols are tracked
       yield put(fetchStockDataSuccess({}));
     } else {
       const symbolsString = action.payload.join(',');
       const url = `https://api.iextrading.com/1.0/stock/market/batch?symbols=${symbolsString}&types=company,chart&range=1y`;
+      // Fetch data from iex api
       const stockData = yield call(axios.get, url);
 
+      // Dispatch success action with data retrieved from iex api
       yield put(fetchStockDataSuccess(stockData.data));
     }
   } catch (e) {
+    // Dispatch failure action if any error was occured
     yield put(fetchStockDataFailure(e.message));
   }
 }
