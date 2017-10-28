6c5ee8cba21157406514d16325d523033a5749e5
Kamyl <kamillcybulski@gmail.com>
2017-10-29 01:28:14 +0200

    Refactor transformData function

--- src/lib/transform-data.js
+++ src/lib/transform-data.js
@@ -1,7 +1,15 @@
 /* eslint no-plusplus: ["error", { "allowForLoopAfterthoughts": true }] */
 
+/**
+ * transformData
+ * @param {object} data Data fetched from iex API
+ * Transform the data into an array of objects with following format:
+ * {date: "27-09-2017", AAPL: 170.63, GOOG: 1019.27}
+ * @returns {array} Array of object with specified format.
+ */
 export default (data) => {
   const keys = Object.keys(data);
+  if (keys.length === 0) return [];
 
   const pricer = stock => stock.chart.reduce((acc, current) =>
     acc.concat([{
@@ -12,19 +20,17 @@ export default (data) => {
 
   const merge = (arr) => {
     if (arr.length === 1) return arr[0];
-
     let result = [];
     const base = arr[0];
-    const iLength = base.length;
-    const jLength = arr.length;
 
-    for (let i = 0; i < iLength; i++) {
-      let obj = base[i];
-      for (let j = 1; j < jLength; j++) {
-        obj = Object.assign(obj, arr[j][i]);
-      }
-      result = result.concat([obj]);
+    while (base.length > 0) {
+      let entry = {};
+      arr.forEach((a) => {
+        entry = Object.assign(entry, a.shift());
+      });
+      result = result.concat([entry]);
     }
+
     return result;
   };
 
