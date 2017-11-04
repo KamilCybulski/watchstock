178881e7d90632fa92f423452133e00c03ba1655
Kamyl <kamillcybulski@gmail.com>
2017-11-04 17:27:04 +0100

    Implement proper loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const Loader = () => (
-  <div className="loader">
-    Loading...
+  <div className="spinner">
+    <div className="rect1" />
+    <div className="rect2" />
+    <div className="rect3" />
+    <div className="rect4" />
+    <div className="rect5" />
   </div>
 );
 
