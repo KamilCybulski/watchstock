6baacea6af324849590ac3d21bbb9fc2ede80c06
Kamyl <kamillcybulski@gmail.com>
2017-11-04 17:45:43 +0100

    Add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,21 @@ import React from 'react';
 
 const Footer = () => (
   <footer className="footer">
-    This is Footer
+    <span className="footer__span">
+      <a href="https://github.com/KamilCybulski/watchstock">
+        @kamilcybulski
+      </a>
+    </span>
+    <span className="footer__span">
+      <a href="http://glyphicons.com/">
+        Made with glyphicons
+      </a>
+    </span>
+    <span className="footer__span">
+      <a href="https://iextrading.com/developer/">
+        Data provided for free by iex
+      </a>
+    </span>
   </footer>
 );
 
