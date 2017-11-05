f9f09433bef8b91fdcebec69fc2f24202add67eb
Kamyl <kamillcybulski@gmail.com>
2017-11-05 18:14:01 +0100

    Refactor reducer tests

--- test/reducer.test.js
+++ test/reducer.test.js
@@ -1,77 +1,131 @@
 import reducer from '../src/reducers/reducer';
+import changePeriodDisplayed from '../src/actions/change-period-displayed';
 import {
   fetchStockDataFailure,
   fetchStockDataSuccess } from '../src/actions/fetch-stock-data';
 import {
   addStockSymbolFailureDB,
   addStockSymbolFailureInvalid } from '../src/actions/add-stock-symbol';
+import {
+  clearAllErrors,
+  symbolTrackedError } from '../src/actions/error-actions';
 
-
-const initialState = {
-  stocks: Object.create(null),
+const state = {
+  stocks: null,
+  timePeriodDisplayed: 365,
   errors: {
     onStockDataFetch: false,
     onStockSymbolSave: false,
     stockDoesntExist: false,
+    symbolAlreadyTracked: false,
   },
 };
 
-const makeNewState = action => reducer(initialState, action);
+const stateWithErrors = {
+  stocks: null,
+  timePeriodDisplayed: 365,
+  errors: {
+    onStockDataFetch: true,
+    onStockSymbolSave: true,
+    stockDoesntExist: true,
+    symbolAlreadyTracked: false,
+  },
+};
+
+describe('Reducer', () => {
+  it('CHANGE_TIME_PERIOD_DISPLAYED action', () => {
+    expect(reducer(state, changePeriodDisplayed(7))).toEqual({
+      stocks: null,
+      timePeriodDisplayed: 7,
+      errors: {
+        onStockDataFetch: false,
+        onStockSymbolSave: false,
+        stockDoesntExist: false,
+        symbolAlreadyTracked: false,
+      },
+    });
+  });
 
 
-describe('Reducer:', () => {
+  it('FETCH_STOCK_DATA_SUCCESS action', () => {
+    expect(reducer(state, fetchStockDataSuccess('aaa'))).toEqual({
+      stocks: 'aaa',
+      timePeriodDisplayed: 365,
+      errors: {
+        onStockDataFetch: false,
+        onStockSymbolSave: false,
+        stockDoesntExist: false,
+        symbolAlreadyTracked: false,
+      },
+    });
+  });
+
+
   it('FETCH_STOCK_DATA_FAILURE action', () => {
-    const action = fetchStockDataFailure('Error Here');
-    const newState = makeNewState(action);
-
-    expect(newState).toEqual({
-      stocks: expect.objectContaining({}),
+    expect(reducer(state, fetchStockDataFailure('some error'))).toEqual({
+      stocks: null,
+      timePeriodDisplayed: 365,
       errors: {
         onStockDataFetch: true,
         onStockSymbolSave: false,
         stockDoesntExist: false,
+        symbolAlreadyTracked: false,
       },
     });
   });
 
-  it('FETCH_STOCK_DATA_SUCCESS action', () => {
-    const action = fetchStockDataSuccess('Some Data');
-    const newState = makeNewState(action);
 
-    expect(newState).toEqual({
-      stocks: 'Some Data',
-      errors: {
-        onStockDataFetch: false,
-        onStockSymbolSave: false,
-        stockDoesntExist: false,
-      },
-    });
-  });
-
-  it('ADD_STOCK_SYMBOL_FAILURE_DB action', () => {
-    const action = addStockSymbolFailureDB();
-    const newState = makeNewState(action);
-
-    expect(newState).toEqual({
-      stocks: expect.objectContaining({}),
+  it('ADD_STOCK_SYMBOL_FAILURE_DB_ERROR action', () => {
+    expect(reducer(state, addStockSymbolFailureDB())).toEqual({
+      stocks: null,
+      timePeriodDisplayed: 365,
       errors: {
         onStockDataFetch: false,
         onStockSymbolSave: true,
         stockDoesntExist: false,
+        symbolAlreadyTracked: false,
       },
     });
   });
 
-  it('ADD_STOCK_SYMBOL_FAILURE_SYMBOL_INVALID action', () => {
-    const action = addStockSymbolFailureInvalid();
-    const newState = makeNewState(action);
 
-    expect(newState).toEqual({
-      stocks: expect.objectContaining({}),
+  it('ADD_STOCK_SYMBOL_FAILURE_SYMBOL_INVALID action', () => {
+    expect(reducer(state, addStockSymbolFailureInvalid())).toEqual({
+      stocks: null,
+      timePeriodDisplayed: 365,
       errors: {
         onStockDataFetch: false,
         onStockSymbolSave: false,
         stockDoesntExist: true,
+        symbolAlreadyTracked: false,
+      },
+    });
+  });
+
+
+  it('CLEAR_ALL_ERRORS action', () => {
+    expect(reducer(stateWithErrors, clearAllErrors())).toEqual({
+      stocks: null,
+      timePeriodDisplayed: 365,
+      errors: {
+        onStockDataFetch: false,
+        onStockSymbolSave: false,
+        stockDoesntExist: false,
+        symbolAlreadyTracked: false,
+      },
+    });
+  });
+
+
+  it('SYMBOL_TRACKED_ERROR action', () => {
+    expect(reducer(state, symbolTrackedError())).toEqual({
+      stocks: null,
+      timePeriodDisplayed: 365,
+      errors: {
+        onStockDataFetch: false,
+        onStockSymbolSave: false,
+        stockDoesntExist: false,
+        symbolAlreadyTracked: true,
       },
     });
   });
